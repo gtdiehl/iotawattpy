5d806df56e7eeee88c65bfb3dae48d4b28ba045a
Greg <greg.diehl.gtd@gmail.com>
2020-12-17 15:31:07 -0800

    Bumping version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="iotawattpy",
-    version="0.0.2",
+    version="0.0.3",
     author="Greg Diehl",
     author_email="greg.diehl.gtd@gmail.com",
     description="Python library for the IoTaWatt Energy device",
